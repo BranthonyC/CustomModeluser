37a3225490dc8ada0b8e7a0ec28e30b941218752
Anthony Chitay <antonchitay@icloud.com>
2020-07-08 13:30:16 -0600

    capitulo 6

--- pages/tests.py
+++ pages/tests.py
@@ -1,6 +1,6 @@
 from django.test import SimpleTestCase
 from django.urls import reverse, resolve
-from .views import HomePageView
+from .views import HomePageView, AboutPageView
 
 
 class HomepageTests(SimpleTestCase):
@@ -29,5 +29,26 @@ class HomepageTests(SimpleTestCase):
         self.assertEqual(
             view.func.__name__, HomePageView.as_view().__name__
         )
+
+class AboutTestPageTest(SimpleTestCase):
+    
+    def setUp(self):
+        self.response = self.client.get(reverse('about'))
+
+    def test_aboutpage_status_code(self):
+        self.assertEqual(self.response.status_code, 200)
+
+    def test_aboutpage_template(self):
+        self.assertTemplateUsed(self.response, 'about.html')
+    
+    def test_aboutpage_contains_correct_html(self):
+        self.assertContains(self.response, 'About Page')
+
+    def test_aboutpage_does_not_contain_incorrect_html(self):
+        self.assertNotContains(self.response, 'Hi there amigo quiero una carnita azada')
+
+    def test_aboutpage_url_resolves_aboutpageview(self):
+        view = resolve('/about/')
+        self.assertEqual(view.func.__name__, AboutPageView.as_view().__name__)
 
 
