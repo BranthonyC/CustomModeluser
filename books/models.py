509ce41c0b4b4f0183cbee64dcfc5f04a8e6656b
Anthony Chitay <antonchitay@icloud.com>
2020-07-09 08:29:24 -0600

    charapter 10 adding the books to store

---
+++ books/models.py
@@ -0,0 +1,17 @@
+from django.db import models
+from django.urls import reverse
+import uuid
+
+class Book(models.Model):
+    id = models.UUIDField(primary_key=True, default=uuid.uuid4, editable=False)
+    title = models.CharField(max_length=200)
+    author = models.CharField(max_length=200)
+    price = models.DecimalField(max_digits=5, decimal_places=2)
+    stock = models.IntegerField(default=0)
+
+    def __str__(self):
+        return "Book: {0} Author: {1}".format(self.title, self.author)
+    
+    def get_absolute_url(self):
+        return reverse("book_detail", args=[str(self.id)])
+    
