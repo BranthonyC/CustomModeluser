0355a9536f9becc6b975574808eabe6e100873f8
Anthony Chitay <antonchitay@icloud.com>
2020-07-08 19:59:18 -0600

    adding enviromental variables capitulo 8

--- bookstore_project/settings.py
+++ bookstore_project/settings.py
@@ -20,10 +20,10 @@ BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 # See https://docs.djangoproject.com/en/3.0/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = '@lgmubqzn^9@u#gr15_ux4w-l8w9*8@wrv_!@hcgq#(qx1m$2_'
+SECRET_KEY = os.environ.get('SECRET_KEY')
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = os.environ.get('DEBUG', default=0)
 
 ALLOWED_HOSTS = []
 
